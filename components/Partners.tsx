7fa93c8aad0a82312bafa5e4c1ccd9af98bf6be9
Anugrah <105841106723@student.unismuh.ac.id>
2026-08-30 19:06:56 +0800

    style: update partners intro text, fix partner-grid to 6 columns

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -3,7 +3,7 @@ const partners = ['Skyline Group', 'Mandala Edu', 'Ruang Cipta', 'Evergreen Corp
 export default function Partners() {
   return (
     <section className="partners container reveal">
-      <p>Dipercaya oleh komunitas traveler, sekolah, dan perusahaan:</p>
+      <p>Dipercaya lebih dari 60 perusahaan dan institusi pendidikan di seluruh Indonesia:</p>
       <div className="partner-grid">
         {partners.map((name) => (
           <span key={name}>{name}</span>
